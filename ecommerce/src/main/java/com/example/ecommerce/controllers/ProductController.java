9582ffaca7656903a9fd5fe34d3d4271f7b634e6
Pedro Tosta <pedrotosta11@gmail.com>
2023-09-13 22:01:40 -0400

    Aula 13-09

--- ecommerce/src/main/java/com/example/ecommerce/controllers/ProductController.java
+++ ecommerce/src/main/java/com/example/ecommerce/controllers/ProductController.java
@@ -1,15 +1,31 @@
 package com.example.ecommerce.controllers;
 
+import com.example.ecommerce.dto.ProductDTO;
+import com.example.ecommerce.entities.Product;
+import com.example.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
+    @Autowired
+    private ProductService service;
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+        return service.findById(id);
+    }
 
     @GetMapping
-    public String teste(){
-        return "Hello world!";
+    public Page<ProductDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
     }
 }
---
+++ ecommerce/src/main/java/com/example/ecommerce/service/ProductService.java
@@ -0,0 +1,33 @@
+package com.example.ecommerce.service;
+
+import com.example.ecommerce.dto.ProductDTO;
+import com.example.ecommerce.entities.Product;
+import com.example.ecommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public ProductDTO findById(Long id){
+        Product product = repository.findById(id).get();
+        return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> product = repository.findAll(pageable);
+        return product.map(x -> new ProductDTO(x));
+    }
+
+}
+
