e25bdd8de96c2ef85d05d9335605988b9b28ecd7
Pedro Tosta <pedrotosta11@gmail.com>
2023-10-18 20:55:16 -0400

    Aula 18-10

--- ecommerce/src/main/java/com/example/ecommerce/controllers/ProductController.java
+++ ecommerce/src/main/java/com/example/ecommerce/controllers/ProductController.java
@@ -2,13 +2,19 @@ package com.example.ecommerce.controllers;
 
 import com.example.ecommerce.dto.ProductDTO;
 import com.example.ecommerce.dto.CustomError;
+import com.example.ecommerce.dto.ValidationError;
 import com.example.ecommerce.entities.Product;
 import com.example.ecommerce.service.ProductService;
 import com.example.ecommerce.service.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -35,7 +41,7 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){
+    public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto){
         /*dto = service.insert(dto);
         return dto;*/
         // se você quiser resumir pode retornar direto assim:
@@ -46,7 +52,7 @@ public class ProductController {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto){
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto){
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
     }
@@ -55,6 +61,19 @@ public class ProductController {
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        for (FieldError f: e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+        /*err.addError("name", "Mensagem de teste");
+        err.addError("price", "Preço inválido");*/
+        return ResponseEntity.status(status).body(err);
     }
 
 }
--- ecommerce/src/main/java/com/example/ecommerce/dto/ProductDTO.java
+++ ecommerce/src/main/java/com/example/ecommerce/dto/ProductDTO.java
@@ -2,14 +2,22 @@ package com.example.ecommerce.dto;
 
 import com.example.ecommerce.entities.Product;
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 public class ProductDTO {
     private Long id;
+    @Size(min = 3, max = 80, message = "Nome precisa ter entre 3 e 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+    @Size (min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
